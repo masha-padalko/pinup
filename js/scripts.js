dadebdd34bff0f7707e58c3d11d37df3eddce51a
masha-padalko <masha_padalko@yahoo.com>
2023-07-06 16:06:21 +0300

    responsive player

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 
-// function isDesktopBrowser() {
-//   let userAgent = navigator.userAgent.toLowerCase(),
-//   isMobile = /mobile|android|iphone|ipod|windows phone/i.test(userAgent),
-//   isTablet = /tablet|ipad/i.test(userAgent);
+function isDesktopBrowser() {
+  let userAgent = navigator.userAgent.toLowerCase(),
+  isMobile = /mobile|android|iphone|ipod|windows phone/i.test(userAgent),
+  isTablet = /tablet|ipad/i.test(userAgent);
   
-//   return !(isMobile || isTablet);
-// }
+  return !(isMobile || isTablet);
+}
 
-// if (isDesktopBrowser()) {
-//   const hero = document.querySelector('.hero'),
-//   player = document.querySelector('.hero__img');
+if (isDesktopBrowser()) {
+  const hero = document.querySelector('.hero'),
+  player = document.querySelector('.hero__img');
 
-//   hero.addEventListener('mousemove', (e) => {
-//     let pageX = e.clientX;
-//     player.style.transform = 'translate('+ pageX/150 +'%, -50%)';
-//   });
-// } 
+  hero.addEventListener('mousemove', (e) => {
+    let pageX = e.clientX;
+    player.style.transform = 'translate('+ pageX/150 +'%, -50%)';
+  });
+} 
