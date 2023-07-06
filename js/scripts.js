a6cd7f7f5f1b2a929897a68513e945bd7129a4b5
masha-padalko <masha_padalko@yahoo.com>
2023-07-06 16:05:51 +0300

    responsive player

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
 
+// function isDesktopBrowser() {
+//   let userAgent = navigator.userAgent.toLowerCase(),
+//   isMobile = /mobile|android|iphone|ipod|windows phone/i.test(userAgent),
+//   isTablet = /tablet|ipad/i.test(userAgent);
+  
+//   return !(isMobile || isTablet);
+// }
 
+// if (isDesktopBrowser()) {
+//   const hero = document.querySelector('.hero'),
+//   player = document.querySelector('.hero__img');
+
+//   hero.addEventListener('mousemove', (e) => {
+//     let pageX = e.clientX;
+//     player.style.transform = 'translate('+ pageX/150 +'%, -50%)';
+//   });
+// } 
