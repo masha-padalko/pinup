dd9e5e7d5472ec0972d91b509fb8a4c8bc532cb3
masha-padalko <masha_padalko@yahoo.com>
2023-07-07 12:18:13 +0300

    readme file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-const mediaQuery = window.matchMedia('(min-width: 1280px)')
-// Check if the media query is true
+// Player mousemove
+const mediaQuery = window.matchMedia('(min-width: 1280px)');
+
 if (mediaQuery.matches) {
-  // Then trigger an alert
   const hero = document.querySelector('.hero'),
   player = document.querySelector('.hero__img');
 
@@ -11,53 +11,28 @@ if (mediaQuery.matches) {
   });
 }
 
-// function isDesktopBrowser() {
-//   let userAgent = navigator.userAgent.toLowerCase(),
-//   isMobile = /mobile|android|iphone|ipod|windows phone/i.test(userAgent),
-//   isTablet = /tablet|ipad/i.test(userAgent);
-  
-//   return !(isMobile || isTablet);
-// }
+// Modal
 
-// if (isDesktopBrowser()) {
-//   const hero = document.querySelector('.hero'),
-//   player = document.querySelector('.hero__img');
-
-//   hero.addEventListener('mousemove', (e) => {
-//     let pageX = e.clientX;
-//     player.style.transform = 'translate('+ pageX/150 +'%, -50%)';
-//   });
-// } 
-
-// Get references to the modal overlay, window, and open button
-const modalOverlay = document.getElementById('modalOverlay');
-const modalWindow = document.getElementById('modalWindow');
-const openModalButton = document.getElementById('openModalButton');
-const closeModalButton = document.getElementById('closeModalButton');
+const modalOverlay = document.getElementById('modalOverlay'),
+    modalWindow = document.getElementById('modalWindow'),
+    openModalButton = document.getElementById('openModalButton'),
+    closeModalButton = document.getElementById('closeModalButton');
 
 function openModal() {
-  // Add class to the body to prevent scrolling
   document.body.classList.add('modal__open');
 
-  // Show the modal overlay and window
   modalOverlay.style.display = 'block';
   modalWindow.style.display = 'block';
 }
 
 function closeModal() {
-  // Remove class from the body to re-enable scrolling
   document.body.classList.remove('modal-open');
 
-  // Hide the modal overlay and window
   modalOverlay.style.display = 'none';
   modalWindow.style.display = 'none';
 }
 
-// Attach click event listener to the open button
 openModalButton.addEventListener('click', openModal);
-
 closeModalButton.addEventListener('click', closeModal);
-
-// Example usage: Close the modal when the overlay is clicked
 modalOverlay.addEventListener('click', closeModal);
 
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
-<h1>Trial Task Padalko Mariia</h1>
+<h1>Test Task Padalko Mariia</h1>
 
 <h2>Github pages:</h2>
-<a href="https://masha-padalko.github.io/test/" target="_blank">https://masha-padalko.github.io/test/</a>
+<a href="https://masha-padalko.github.io/pinup/" target="_blank">https://masha-padalko.github.io/pinup/</a>
 <h2>Project Setup:</h2>
 <p>npm install</p>
 <p>gulp serve</p>
@@ -9,24 +9,32 @@
 <p>https://nodejs.org/download/release/v14.18.0/</p>
 
 <h2>Comments:</h2>
-<p>Used slider  <a href="https://kenwheeler.github.io/slick/" target="_blank">Slick carousel</a></p>
+<p>Mobile first approach</p>
 <p>All svg were compressed by <a href="https://jakearchibald.github.io/svgomg/" target="_blank">SVGOMG</a> service</p>
 <p>PNG images were compressed by <a href="https://compress-or-die.com/" target="_blank">Compress or die</a> service</p>
 
 <h2>Tested on mobile devices:</h2>
-<p>Iphone 8 Safari browser</p>
 <p>Iphone 12 PRO Max Safari browser</p>
+<img src="img/screenshots/iphone12_.jpg">
+<img src="img/screenshots/iphone12.jpg">
 <p>Iphone 14 PRO Safari browser</p>
+<img src="img/screenshots/iphone14.jpg">
 <p>Xiaomi mi9 X browser</p>
+<img src="img/screenshots/android.jpg">
 
 <h2>Troubleshooting:</h2>
 <p>Responsive player</p>
+<p>Bottom smoke animation (create bigger image: add mirror of current one, cause image from mockup does not fit animation from video example)</p>
 
 <h2>Lack of knowledge:</h2>
-<p>Resposive units of measurement</p>
+<p>Responsive units of measurement</p>
 
 <h2>What could be improve:</h2>
-<p>Add GULP task for prefixes</p>
-<p>Add markup for more breakpoints</p>
-<p>Quality of mobile player</p>
+<p>Add gulp task for prefixes</p>
+<p>Add markup for more breakpoints or use responsive units of measurement</p>
+<p>Quality of mobile player image</p>
+<p>Add 2x images for Retina display and add html css params</p>
+<p>Add active state for buttons</p>
+<p>Fix svg icon font for second icon (it cutted on the right bottom corner after font generating)</p>
+<p>Find better inage for smoke and create more smooth animation</p>
 
